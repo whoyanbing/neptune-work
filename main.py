d036bbd3438934cbc16be2881b3b1d5c399bcf35
whoyanbing <whoyanbing@MacBookAir.local>
2019-08-06 16:18:21 +0800

    0806-2

--- main.py
+++ main.py
@@ -88,8 +88,8 @@ def load_product_reference(filepath, graph_traversal):
 
 def main():
     print('load start!')
-    #remote_server = 'ws://localhost'
-    remote_server = 'wss://recengineonpremdatasource.comltq8nzp9d.us-west-2.neptune.amazonaws.com'
+    remote_server = 'ws://localhost'
+    #remote_server = 'wss://recengineonpremdatasource.comltq8nzp9d.us-west-2.neptune.amazonaws.com'
     remote_conn = graph_connect(remote_server)
     g_traversal = graph_traversal(remote_conn)
     file_list = file_path_list('data/purchase_history')
